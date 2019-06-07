b1eee62496cef417ecec28912082eb12ec8dd162
richardjtelford <richard.telford@bio.uib.no>
2019-06-08 01:29:00 +0200

    white space

--- R/obs.cor.R
+++ R/obs.cor.R
@@ -22,31 +22,38 @@
 #N2 of fossil/training/joint
 
 
-obs.cor<-function (spp, env, fos, ord = rda, n=99, min.occur=1){
-    spp<-spp[,colSums(spp>0)>=min.occur]
-    fos<-fos[,colSums(fos>0)>=min.occur]
-    
-    spp<-spp[,order(colnames(spp))]
-    fos<-fos[,order(colnames(fos))]
- 
-    mod <- WA(spp, env)
-    pred <- predict(mod, fos)$fit[, 1]
-    RDA <- ord(fos ~ pred)
-    optima <- mod$coef                                      
-    sco <- scores(RDA, display = "spec", choice = 1)
-    
-    abun.fos <- colMeans(fos)[intersect(names(fos), names(spp))]
-    abun.calib <- colMeans(spp)[intersect(names(spp), names(fos))]
-    abun.joint<-abun.fos*abun.calib
-    n2.fos<-Hill.N2(fos, margin=2)[names(fos)%in%names(spp)]
-    n2.calib<-Hill.N2(spp, margin=2)[names(spp)%in%names(fos)]
-    n2.joint<-n2.fos*n2.calib
-    abundances<-data.frame(abun.fos=abun.fos, abun.calib=abun.calib, abun.joint=abun.joint, n2.fos=n2.fos, n2.calib=n2.calib, n2.joint=n2.joint)
-
+obs.cor<-function (spp, env, fos, ord = rda, n = 99, min.occur = 1){
+  spp <- spp[, colSums(spp > 0) >= min.occur]
+  fos <- fos[, colSums(fos > 0) >= min.occur]
+  
+  spp <- spp[, order(colnames(spp))]
+  fos <- fos[, order(colnames(fos))]
+  
+  mod <- WA(spp, env)
+  pred <- predict(mod, fos)$fit[, 1]
+  RDA <- ord(fos ~ pred)
+  optima <- coef(mod)
+  sco <- scores(RDA, display = "spec", choice = 1)
+  
+  abun.fos <- colMeans(fos)[intersect(names(fos), names(spp))]
+  abun.calib <- colMeans(spp)[intersect(names(spp), names(fos))]
+  abun.joint <- abun.fos * abun.calib
+  n2.fos <- Hill.N2(fos, margin = 2)[names(fos) %in% names(spp)]
+  n2.calib <- Hill.N2(spp, margin = 2)[names(spp) %in% names(fos)]
+  n2.joint <- n2.fos * n2.calib
+  abundances <- data.frame(
+      abun.fos = abun.fos,
+      abun.calib = abun.calib,
+      abun.joint = abun.joint,
+      n2.fos = n2.fos,
+      n2.calib = n2.calib,
+      n2.joint = n2.joint
+    )
+  
     optima <- optima[intersect(rownames(optima), rownames(sco)), , drop = FALSE]
     sco <- sco[intersect(rownames(sco), rownames(optima)), , drop = FALSE]
     x <- data.frame(optima, sco, abundances)
-    wcs<-sapply(abundances,function(abun)abs(cov.wt(x[, 1:2], wt = abun, cor = TRUE)$cor[1, 2]))
+    wcs <- sapply(abundances,function(abun)abs(cov.wt(x[, 1:2], wt = abun, cor = TRUE)$cor[1, 2]))
     
     res.obs <- list(x = x, res = c(wcs, unweighted = abs(cor(x[, 1:2])[1, 2])))
 
@@ -59,14 +66,14 @@ obs.cor<-function (spp, env, fos, ord = rda, n=99, min.occur=1){
         optima <- optima[intersect(rownames(optima), rownames(sco)), , drop = FALSE]
         sco <- sco[intersect(rownames(sco), rownames(optima)), , drop = FALSE]
         x <- data.frame(optima, sco)
-        wcs<-sapply(abundances,function(abun)abs(cov.wt(x[, 1:2], wt = abun, cor = TRUE)$cor[1, 2]))
+        wcs <- sapply(abundances,function(abun)abs(cov.wt(x[, 1:2], wt = abun, cor = TRUE)$cor[1, 2]))
 
         c(wcs, unweighted = abs(cor(x[, 1:2])[1, 2]))
     })
-    res.sim<-as.data.frame(t(res.sim))
-    sigs<-mapply(function(ob,sim)mean(ob<=c(ob,sim)), ob=res.obs$res, sim=res.sim)
+  res.sim <- as.data.frame(t(res.sim))
+  sigs <- mapply(function(ob, sim)mean(ob <= c(ob, sim)), ob = res.obs$res, sim = res.sim)
     
-    res<-list(ob=res.obs, sim=res.sim, sigs=sigs)
-    class(res)<-"obscor"
-    return(res)
+  res <- list(ob = res.obs, sim = res.sim, sigs = sigs)
+  class(res) <- "obscor"
+  return(res)
 }
