abc859ab7fc2a6eb35a5a976572c96f56156f4c1
richardjtelford <richard.telford@bio.uib.no>
2019-06-08 11:11:30 +0200

    tidied up code

--- R/randomTF.R
+++ R/randomTF.R
@@ -1,73 +1,111 @@
-randomTF <-
-function(spp,env,fos,n=99,fun,col, condition, autosim, ord=rda,...){#reconstruct random variables
-  if(!is.list(env))env<-list(env=env)
-  rownames(spp)<-1:nrow(spp)
-  if(identical(names(formals(MAT)),names(formals(fun)))){
-      mod1<-predict(MAT(spp,env[[1]],...),fos)
-      analogues<-unique(as.vector(as.numeric(mod1$match.name)))
-      spp<-spp[analogues,]
-      env<-env[analogues,,drop=FALSE]
-      rownames(spp)<-1:nrow(spp)
+
+#' @importFrom rioja MAT
+#' @importFrom purrr map map_dbl
+#' @importFrom tibble lst
+#' @export
+
+randomTF <- function(spp, env, fos, n = 99, fun, col,
+                     condition, autosim, ord = rda, ...){
+  #reconstruct random variables
+  if (!is.list(env)){
+    env <- list(env = env)
   }
-  partial<-!missing(condition)
+  rownames(spp) <- 1:nrow(spp)
   
-  if(!partial){
-    PC<-ord(fos)
-  }else{
-    form1<-formula(paste("fos~1+Condition(",paste(names(condition), collapse="+"),")"))
-    PC<-ord(form1, data=condition)
+  #if MAT, for speed, drop training set samples that are never analogues.
+  if (identical(fun, MAT)) {
+    mod1 <- predict(MAT(spp, env[[1]], ...), fos)
+    analogues <- unique(as.vector(as.numeric(mod1$match.name)))
+    spp <- spp[analogues, ]
+    env <- env[analogues, , drop = FALSE]
+    rownames(spp) <- 1:nrow(spp)
   }
-  MAX<- PC$CA$eig[1]/PC$tot.chi
   
-  obs<-lapply(env,function(ev){
-    Mod<-fun(spp, ev,...)
-    Pred<-predict(Mod,fos)
-    if(is.list(Pred))p<-Pred$fit[,col]
-    else p<-Pred
-    if(!partial){
-      RDA<-ord(fos~p)
-    }else{
-      form<-formula(paste("fos~p+Condition(",paste(names(condition), collapse="+"),")"))
-      RDA<-ord(form, data=condition)
+  partial <- !missing(condition)
+  
+  #find inertia explained by first axis of unconstrained ordination
+  if (!partial) {
+    PC <- ord(fos)
+  } else{
+    conditions <- paste(names(condition), collapse = "+")
+    form1 <- formula(paste("fos ~ 1 + Condition(", conditions, ")"))
+    PC <- ord(form1, data = condition)
+  }
+  MAX <- PC$CA$eig[1] / PC$tot.chi
+  
+  # Find inertia explained by reconstructions
+  obs <- lapply(env, function(ev) {
+    Mod <- fun(spp, ev, ...)
+    Pred <- predict(Mod, fos)
+    if (is.list(Pred)) {
+      p <- Pred$fit[, col]
     }
-    EX<- RDA$CCA$tot.chi/RDA$tot.chi
-    EIG1<-RDA$CA$eig[1]/RDA$tot.chi
-    list(EX=EX, pred=p, EIG1=EIG1, clas=class(Mod), mod=Pred)
+    else {
+      p <- Pred
+    }
+    if (!partial) {
+      RDA <- ord(fos ~ p)
+    } else{
+      form <- formula(paste("fos ~ p + Condition(", conditions, ")"))
+      RDA <- ord(form, data = condition)
+    }
+
+    list(
+      EX = RDA$CCA$tot.chi / RDA$tot.chi,
+      pred = p,
+      EIG1 = RDA$CA$eig[1] / RDA$tot.chi,
+      mod = Pred
+    )
   })
-  if(missing(autosim))rnd<-matrix(runif(nrow(spp)*n),ncol=n)
-  else rnd<-autosim
-  if(obs[[1]]$clas=="MAT"){
-         p<-t(apply( apply(obs[[1]]$mod$m,2,as.numeric),1,function(n)colMeans(rnd[n,])))
-         sim.ex<-apply(p,2,function(pp){
-            if(!partial){
-                r<-ord(fos~pp)
-            }else{
-                form<-formula(paste("fos~pp+Condition(",paste(names(condition), collapse="+"),")"))
-                r<-ord(form, data=condition)
-            }
-            r$CCA$tot.chi/r$tot.chi                      
-         })
+
+  # simulations using random data  
+  if (missing(autosim)) {
+    rnd <- matrix(runif(nrow(spp) * n), ncol = n)
   }
-  else{
-    sim.ex<-apply(rnd,2,function(sim){
-         m<-fun(spp, sim,...)
-         p<-predict(m,fos)
-          if(is.list(p))p<-p$fit[,col]
-         if(!partial){
-            r<-ord(fos~p)
-         }else{
-            form<-formula(paste("fos~p+Condition(",paste(names(condition), collapse="+"),")"))
-            r<-ord(form, data=condition)
-         }
-         r$CCA$tot.chi/r$tot.chi
+  else {
+    rnd <- autosim
+  }
+  if (identical(fun, MAT)) {
+    #if MAT, can take shortcut as always same analogues chosen
+    selected_analogues <- apply(obs[[1]]$mod$match.name, 2, as.numeric)
+    p <- apply(selected_analogues, 1, function(n){
+        colMeans(rnd[n, ])})
+    sim.ex <- apply(p, 1, function(pp) {
+      if (!partial) {
+        r <- ord(fos ~ pp)
+      } else{
+        form <- formula(paste("fos ~ pp + Condition(", conditions, ")"))
+        r <- ord(form, data = condition)
+      }
+      r$CCA$tot.chi / r$tot.chi
     })
   }
-  preds<-lapply(obs,function(x)x$pred)
-  EXs<-sapply(obs,function(x)x$EX)
-  eig1<-sapply(obs,function(x)x$EIG1)
-  sig<-sapply(EXs,function(E)mean(E<=c(E,sim.ex)))
-  res<-list(PCA=PC,preds=preds, MAX=MAX, EX=EXs, eig1=eig1,sim.ex=sim.ex, sig=sig)
-  class(res)<-"palaeoSig"
+  else{
+    sim.ex <- apply(rnd, 2, function(sim) {
+      m <- fun(spp, sim, ...)
+      p <- predict(m, fos)
+      if (is.list(p))
+        p <- p$fit[, col]
+      if (!partial) {
+        r <- ord(fos ~ p)
+      } else{
+        form <- formula(paste("fos ~ p + Condition(", conditions, ")"))
+        r <- ord(form, data = condition)
+      }
+      r$CCA$tot.chi / r$tot.chi
+    })
+  }
+
+  res <- lst(
+      PCA = PC,
+      preds = map(obs, "pred"),
+      MAX = MAX,
+      EX = map_dbl(obs, "EX"),
+      eig1 = map_dbl(obs, "EIG1"),
+      sim.ex = sim.ex,
+      sig = map_dbl(EX, function(E) mean(E <= c(E, sim.ex)))
+    )
+  class(res) <- "palaeoSig"
   return(res)
 }
 
