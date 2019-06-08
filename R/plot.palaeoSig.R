e0be1642b2aaa3b745ed61f5564a770898148b5c
richardjtelford <richard.telford@bio.uib.no>
2019-06-08 13:20:26 +0200

    autoplot and reverse p scale

--- R/plot.palaeoSig.R
+++ R/plot.palaeoSig.R
@@ -2,7 +2,7 @@
 #' @importFrom stats quantile
 #' @importFrom TeachingDemos spread.labs
 #' 
-plot.palaeoSig <- function(x, vnames, top = 0.7, adj = c(0,0.5), p.val = 0.95, ...){
+plot.palaeoSig <- function(x, vnames, top = 0.7, adj = c(0,0.5), p.val = 0.05, ...){
   if (missing(vnames)) {
     vnames <- names(x$EX)
   }
@@ -16,7 +16,7 @@ plot.palaeoSig <- function(x, vnames, top = 0.7, adj = c(0,0.5), p.val = 0.95, .
       lines(rep(z, 2), c(0, tops)))
     #abline(v=MAX, col=1, lwd=2, lty=3)
     lines(rep(MAX, 2), c(0, tops), col = 1, lwd = 2, lty = 3)
-    lines(rep(quantile(sim.ex,p.val), 2), c(0, tops), 
+    lines(rep(quantile(sim.ex, probs = 1 - p.val), 2), c(0, tops), 
           col = 2, lwd = 1, lty = 3)
     #abline(v=EX, col=1)     
     putEX <- spread.labs(EX, 1.2 * strwidth('A', cex = .8))
@@ -25,3 +25,49 @@ plot.palaeoSig <- function(x, vnames, top = 0.7, adj = c(0,0.5), p.val = 0.95, .
          srt = 90, adj = adj, cex = .8)
   })
 }
+
+#' @importFrom   ggplot2 autoplot ggplot aes geom_col geom_linerange geom_text scale_colour_identity scale_linetype_identity labs
+#' @importFrom tibble tibble
+#' @importFrom rlang .data
+
+autoplot.palaeoSig <- function(x, variable_names, nbins = 20, top = 0.7, p.val = 0.05){
+  if (missing(variable_names)) {
+    variable_names <- names(x$EX)
+  }
+  
+  breaks <- seq(min(x$sim.ex), max(x$sim.ex), length = nbins + 1)
+  id <- cut(x$sim.ex, breaks = breaks, include.lowest = TRUE)
+  
+  sim_bin <- tibble(  
+    mid_point = (breaks[-length(breaks)] + breaks[-1]) / 2,
+    n = as.vector(table(id))
+  )
+
+  width <- diff(sim_bin$mid_point[1:2])
+  
+  lines_to_add <- tibble(
+    label = c("PC1", paste("p =", p.val), variable_names), 
+    value = c(x$MAX, quantile(x$sim.ex, probs = 1 - p.val), x$EX), 
+    max = max(sim_bin$n) * top, 
+    linetype = c("dashed", "dotted", rep("solid", length(variable_names))),
+    colour = c("black", "red", rep("black", length(variable_names)))
+  )
+    
+  g <- ggplot(sim_bin, aes(x = .data$mid_point, y = .data$n)) +
+    geom_col(fill = "grey70", width = width) +
+    geom_linerange(data = lines_to_add, 
+                  aes(x = .data$value, ymin = 0, ymax = .data$max, linetype = .data$linetype, colour = .data$colour), 
+                  inherit.aes = FALSE) +
+    geom_text(data = lines_to_add, 
+                   aes(x = .data$value, y = .data$max, label = .data$label,
+                       colour = .data$colour),
+           # method = "top.qp",
+            angle = 90, hjust = 0) +
+    scale_colour_identity() +
+    scale_linetype_identity() +
+    xlim(0, NA) +
+    labs(x = "Proportion variance explained", y = "Frequency")
+  
+  return(g)
+}
+
