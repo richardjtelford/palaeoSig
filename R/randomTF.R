00de5e32f36b04115cd5a84bd3505797ea2ec564
richardjtelford <richard.telford@bio.uib.no>
2019-06-19 10:41:38 +0200

    roxygen added. sanity checks on inputs

--- R/randomTF.R
+++ R/randomTF.R
@@ -1,3 +1,53 @@
+#' @title Proportion of variance in the fossil data explained by an environmental reconstruction
+#' @description Calculate the proportion of variance in the fossil data explained by an environmental reconstruction with a constrained ordination. This value is compared with a null distribution calculated as the proportion of variance in the fossil data explained by reconstructions from transfer functions trained on random data.
+
+#' @param spp Data frame of modern training set species data, transformed as required for example with sqrt
+#' @param env Data frame of training set environmental variables or vector with single environmental variable
+#' @param fos Data frame of fossil species data, with same species codes and transformations as spp
+#' @param n number of random training sets. More is better.
+#' @param fun Transfer function method. Additional arguments to \code{fun} can be passed with \code{...}
+#' @param col Some transfer functions return more than one column of results, for example with different \code{\link[rioja]{WAPLS}} components. \code{col} selects which column to use. See the relevant transfer function method help file.
+#' @param condition Optional data frame of reconstructions to partial out when testing if multiple independent reconstructions are possible.
+#' @param autosim Optional data frame of random values. This is useful if the training set is spatially autocorrelated and the supplied data frame contains autocorrelated random variables. If \code{autosim} is missing, and \code{permute} is \code{FALSE}, the transfer functions are trained on random variables drawn from a uniform distribution.
+#' @param ord Constrained ordination method to use. \code{\link[vegan]{rda}} is the default, \code{\link[vegan]{cca}} should also work. \code{\link[vegan]{capscale}} won't work without modifications to the code (or a wrapper).
+#' @param permute logical value. Generate random environmental variables by permuting existing variable. Only possible if there is only one environmental varible and \code{autosim} is missing.
+#' @param \dots Other arguments to the transfer function. For example to change the distance metric in \code{\link[rioja]{MAT}}. Also extra arguments to plot.
+
+#' @details The function calculates the proportion of variance in the fossil data explained by the transfer function reconstruction. This is compared with a null distribution of the proportion of variance explained by reconstructions based on random environmental variables. Reconstructions can be partialled out to test if multiple reconstructions are statistically significant. If the environment is spatially autocorrelated, a red-noise null should be used instead of the default white noise null. The red noise environmental variables can be generated with the \pkg{gstat} package.
+#' 
+#' Any transfer function in the \pkg{rioja} package can be used. Other methods (e.g. random forests) can be used by making a wrapper function.
+#' 
+#' If several reconstructions using the same training set are being tested, it can be much faster to make the models once, and use them repeatedly. This can be done with \code{ModelMaker} and \code{randomTFmm}. \code{ModelMaker} does not work with MAT.
+#' 
+#' @return 
+#' A list with components
+#' \itemize{
+#'    \item{PCA}{ The unconstrained ordination of the fossil data.}
+#'    \item{preds}{ A list of the containing the reconstructions for each environmental variable.}
+#'    \item{MAX}{ Proportion of the variance explained by the first axis of the unconstrained ordination. This is the maximum amount that a reconstruction of a single variable can explain.}
+#'    \item{EX}{ The proportion of the variance in the fossil data explained by each reconstruction.}
+#'    \item{sim.ex}{ The proportion of variance explained by each of the random environmental variables.}
+#'    \item{sig}{ The p-value of each reconstruction.}
+#'}    
+#'    ModelMaker returns a list of models.
+#'
+#'    \code{autoplot.palaeoSig} returns a \code{ggplot2} object    
+    
+#' @references Telford, R. J. and Birks, H. J. B. (2011) A novel method for assessing the statistical significance of quantitative reconstructions inferred from biotic assemblages. \emph{Quaternary Science Reviews} \bold{30}: 1272--1278. DOI: \href{10.1016/j.quascirev.2011.03.002}{https://doi.org/10.1016/j.quascirev.2011.03.002}
+#' @author Richard Telford \email{richard.telford@uib.no}
+#' @note If there are only a few fossil levels, \code{\link{obs.cor}} might have more power. If there are few taxa, tests on \code{\link[rioja]{MAT}} reconstructions have more statistical power than those based on \code{\link[rioja]{WA}}.
+#' @seealso \code{\link{obs.cor}}, \code{\link[rioja]{WA}}, \code{\link[rioja]{MAT}}, \code{\link[rioja]{WAPLS}}, \code{\link[vegan]{rda}}, \code{\link[vegan]{cca}} 
+#' @examples 
+#'     require(rioja)
+#'     data(SWAP)
+#'     data(RLGH)
+#'     rlghr <- randomTF(spp = sqrt(SWAP$spec), env = data.frame(pH = SWAP$pH),
+#'      fos = sqrt(RLGH$spec), n = 99, fun = WA, col = 1)
+#'    rlghr$sig
+#'    plot(rlghr, "pH")
+#'    autoplot(rlghr, "pH")
+    
+#' @keywords multivariate htest hplot
 
 #' @importFrom rioja MAT
 #' @importFrom purrr map map_dbl
@@ -8,13 +58,26 @@
 
 randomTF <- function(spp, env, fos, n = 99, fun, col,
                      condition, autosim, ord = rda, 
-                     permute = FALSE, ...){
-  #reconstruct random variables
+                     permute = FALSE,
+                     models,
+                     make_models = FALSE, ...){
+
+  #Check env is data.frame or vector
+  if(!is.data.frame(env) & !is.vector(env)){
+    stop("env must be a data.frame containing one or more environemental variables, or vector containing a single environemental variable")
+  }
+  
+  #force data to be a data.frame
   if (!is.data.frame(env)){
     env <- data.frame(env = env)
   }
   rownames(spp) <- 1:nrow(spp)
 
+  #check env and spp have same number of rows
+  if(!identical(nrow(spp), nrow(env))){
+    stop("spp and env must have same number of rows")
+  }
+  
   #permute and autosim don't play together
   if(isTRUE(permute) & !missing(autosim)){
     stop("permute does not make sense if autosim is provided")
@@ -25,7 +88,28 @@ randomTF <- function(spp, env, fos, n = 99, fun, col,
     stop("permute is only possible with one environmental variable")
   }
   
-  #if MAT, for speed, drop training set samples that are never analogues.
+  #check condition is data.frame and fos have same number of rows
+  partial <- !missing(condition)
+  if(partial){
+    if(!is.data.frame(condition)){
+      stop("condition must be a data.frame of reconstructions to partial out")
+    }
+    if(!idential(nrow(fos), nrow(condition))){
+      stop("fos and condition must have the same number of rows")
+    }
+  }
+  
+  
+  #MAT and make_models don't work well together
+  if(identical(fun, MAT) & isTRUE(make_models)){
+    stop("MAT and make_models don't work together because of a shortcut is used to speed up MAT")
+  }
+  
+
+  
+  
+  
+    #if MAT, for speed, drop training set samples that are never analogues.
   if (identical(fun, MAT)) {
     mod1 <- predict(MAT(spp, env[[1]], ...), fos)
     analogues <- unique(as.vector(as.numeric(mod1$match.name)))
@@ -34,7 +118,7 @@ randomTF <- function(spp, env, fos, n = 99, fun, col,
     rownames(spp) <- 1:nrow(spp)
   }
   
-  partial <- !missing(condition)
+
   
   #find inertia explained by first axis of unconstrained ordination
   if (!partial) {
