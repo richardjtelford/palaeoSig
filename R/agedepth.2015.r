2a79e44e5aa561cb5a6a92b0062498a47e8e8a02
RichardJTelford <Richard.Telford@uib.no>
2025-07-28 18:56:29 +0200

    agelme -> roxygen

--- R/agedepth.2015.r
+++ R/agedepth.2015.r
@@ -1,5 +1,65 @@
-#' @export
+#' @title Estimation of the relationship between Calibrated age and depth
+#' @description
+#' Estimates the relationship of Calibrated age and depth for palaeorecords.
+#' The function uses a smooth spline from the mgcv library by Simon Wood.
+#' It produces predicted confidence interval for the relationship approximating
+#' a mixed effect model, as there are two levels of uncertainty,
+#' i.e. within dated object and between dated objects.
 #'
+#' @param depup The upper depths of the dated slides
+#' @param depdo The lower depths of the dated slides
+#' @param bpup The younger calibrated ages of the dated slides
+#' @param bpdo The older calibrated ages of the dated slides
+#' @param use Logical vector of dates to include in the model.
+#' Default is to use all.
+#' @param weights Weights to be used for the estimation, default is fixed
+#' top-layer followed by inverse variance of within dated object
+#' @param vspan The span to be used for the diagnostic plots, default span = 1
+#' @param k Number of base function to start the shrinkage in the
+#' gam estimation procedure
+#' @param m The order of penalty for the term, i.e. the degree of continuity at
+#' the knots (default, m = 2 gives cubic smooth spline)
+#' @param diagnostic Logical, should diagnostic plots be made.
+#'
+#' @details
+#' Note that the fixation of the top layer is done by a weight = 1, whereas the
+#' other weights follows inverse variance within object.
+#'
+#' The diagnostic plots is used to check the quality of the estimation and to
+#' see if there is a need for an assumption of between object variance
+#' proportional to mean.
+#' The latter however is rarely encountered for palaeodata.
+#'
+#' @returns
+#' A list of class 'agelme' with the following items:
+#'
+#' \item{tdf }{Degrees of freedom used by the cubic smooth spline, a vector with first value for constant variance and second vector for variance equal to mu.}
+#' \item{weights }{A vector of the weights used by the cubic smooth spline }
+#' \item{Constant}{A matrix with the numerical results for the dated points using a constant variance}
+#' \item{Muvar}{A matrix with the numerical results for the dated points using variance equal to mu}
+#' \item{RES}{A vector of the Residual sum of squares}
+#' \item{Models}{A list with the models from the cubic smooth spline, constant and mu variance, respectively}
+#' \item{Data}{A data.frame including the data used for the estimation}
+#'
+#' @references
+#' Heegaard, E., Birks, HJB. & Telford, RJ. 2005. Relationships between
+#' calibrated ages and depth in stratigraphical sequences: an estimation
+#' procedure by mixed-effect regression. _The Holocene_ **15**: 612-618
+#' \doi{10.1191/0959683605hl836rr}
+#'
+#' @author Einar Heegaard
+#' @importFrom mgcv gam s
+#' @examples
+#' data(STOR)
+#'
+#' fit.mod <- with(STOR, agelme(depthup, depthdo, cageup, cagedo))
+#'
+#' # Predicting using the constant variance model,
+#' # for each cm between 70 and 400 cm.
+#' fit.pre <- predict(fit.mod, 1, 70:400)
+#' plot(fit.pre)
+#' @export
+
 "agelme" <- function(depup, depdo, bpup, bpdo, use,
                      weights = c(1, rep(0, length(depup) - 1)),
                      vspan = 1, k = length(depup) - 1,
@@ -77,13 +137,55 @@
   age_res
 }
 
+
+#############################################
+#' @title Predicts the Calibrated age for agelme models
+#' @description
+#' This function uses the output from 'agelme' to predict the Calibrated ages
+#' for specified depths.
+#'
+#' @param object An `agelme` model
+#' @param v Using constant (1) or mu (2) variance
+#' @param depth A vector of the depths to be predicted
+#' @param \dots Other arguments, currently unused.
+#' @returns
+#'  A list with three items
+#'  \describe{
+#'  \item{v}{ Whether constant variance or mu variance used.}
+#'  \item{fit}{ A data.frame with the model predictions, containing columns:
+#' \describe{
+#'   \item{Depth}{ The depths for the predicted ages}
+#'   \item{Estage}{ Predicted age}
+#'   \item{Lowlim}{ Lower 95 % confidence interval}
+#'   \item{Upplim}{ Upper 95 % confidence interval}
+#'   \item{Tsd}{ Total standard deviation}
+#' }
+#' }
+#' }
+#' \item{data}{ A data.frame containing the age and depth information of the radiocarbon dates.}
+#' @references
+#' Heegaard, E., Birks, HJB. & Telford, RJ. 2005.
+#' Relationships between calibrated ages and depth in stratigraphical sequences:
+#' an estimation procedure by mixed-effect regression. _The Holocene_ **15**: 612-618
+#' \doi{10.1191/0959683605hl836rr}
+#' @author Einar Heegaard
+#' @examples
+#' data(STOR)
+#'
+#' fit.mod <- with(STOR, agelme(depthup, depthdo, cageup, cagedo))
+#'
+#' # Predicting using the constant variance model,
+#' # for each cm between 70 and 400 cm.
+#' fit.pre <- predict(fit.mod, 1, 70:400)
+#' plot(fit.pre)
 #' @export
+
 "predict.agelme" <- function(object, v = 1, depth, ...) {
   # Cagenew.fun version 1.4R,26.09.05,
   # written by Einar Heegaard, Bjerknes Centre for Climate Research,
   # University of Bergen, Allegaten 55, 5007 Bergen, Norway
   # e.mail: einar.heegaard@bio.uib.no
-  # Use of this function is your responibility
+  # Use of this function is your responsibility
   depup <- object$data[object$data$use, 1]
   depdo <- object$data[object$data$use, 2]
   bpup <- object$data[object$data$use, 3]
